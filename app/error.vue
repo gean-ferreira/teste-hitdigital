84aac1139ab89808ef6138ef1ce167872e6aade7
Gean Ferreira <gean_roberto@hotmail.com>
2026-04-01 13:26:52 -0300

    feat: add página de erro global (404/500)

---
+++ app/error.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps<{
+  error: {
+    statusCode: number
+    message: string
+  }
+}>()
+
+function handleError() {
+  clearError({ redirect: '/' })
+}
+</script>
+
+<template>
+  <main>
+    <h1>{{ error.statusCode }}</h1>
+    <p>{{ error.message }}</p>
+    <button @click="handleError">Voltar para a home</button>
+  </main>
+</template>
