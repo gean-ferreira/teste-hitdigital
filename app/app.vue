7206ab02327562ed09c107d2965f65cfcf8742b5
Gean Ferreira <gean_roberto@hotmail.com>
2026-04-01 15:13:09 -0300

    feat: add layout global com header

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtPage />
-  </div>
+  <UApp>
+    <header class="border-b border-gray-800">
+      <UContainer class="flex items-center justify-between py-4">
+        <NuxtLink to="/" class="text-xl font-bold text-white">
+          Hit Digital
+        </NuxtLink>
+      </UContainer>
+    </header>
+
+    <UContainer class="py-8">
+      <NuxtPage />
+    </UContainer>
+  </UApp>
 </template>
