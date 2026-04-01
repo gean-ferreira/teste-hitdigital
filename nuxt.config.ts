0b0fc310cf3bf3abef5665e8f1bacf548dea9717
Gean Ferreira <gean_roberto@hotmail.com>
2026-04-01 15:12:29 -0300

    chore: add Nuxt UI, Tailwind e cfg dark mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
+  colorMode: {
+    preference: 'dark',
+    fallback: 'dark'
+  },
+  css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
       apiBase: 'https://jsonplaceholder.typicode.com',
